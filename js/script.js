d4588d332fedda5b63ee17207b5ac22fa373b4b9
inventivetalent <inventivetalent@inventivegames.de>
2016-12-23 21:26:07 +0100

    New Website design, yay!

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('.page-scroll a').bind('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function () {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // http://stackoverflow.com/questions/32331471/writing-my-age-in-decimal-format
+    setInterval(function () {
+        var exactAge = ((function (d1, d2) {
+            var diffDays, oneDay;
+            oneDay = 24 * 60 * 60 * 1000;
+            diffDays = (d2 - Date.parse(d1)) / oneDay;
+            return diffDays;
+        })("March 10, 1999", new Date()) / 365);
+        $("#age").text(Math.round(exactAge * 1000) / 1000);
+        $("#age").attr("data-age", exactAge);
+    }, 1000);
+
+    var quotes = [
+        "If you're homophobic you should probably leave now and get some help."
+    ];
+    $("#randomQuotes").text(quotes[Math.floor(Math.random() * quotes.length)]);
+});
